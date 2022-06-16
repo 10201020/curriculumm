41e16b6be1796a2cb55a06530f45af372518f895
10201020 <76522401+10201020@users.noreply.github.com>
2022-06-16 09:21:18 +0900

    4-2
    4章チェックテスト確認お願いします

--- 4-2/index.php
+++ 4-2/index.php
@@ -2,8 +2,10 @@
 require_once("pdo.php");
 require_once("getData.php");
 
-echo $users_data;
-echo $post_data;
+$data = new getData();
+$getUserData = $data->getUserData();
+$getPostData = $data->getPostData();
+
 ?>
 
 <!DOCTYPE html>
@@ -22,8 +24,8 @@ echo $post_data;
         </div>
         <nav>
             <ul class="nav_list">
-                <li class="name">ようこそ●●●●さん</li>
-                <li class="date">最終ログイン日：○○○○</li>
+                <li class="name">ようこそ<?php echo $getUserData["last_name"].$getUserData["first_name"];?>さん</li>
+                <li class="date">最終ログイン日：<?php echo $getUserData["last_login"]?></li>
             </ul>
         </nav>
     </header>
@@ -31,26 +33,34 @@ echo $post_data;
     <main>
 
     <table>
-  <tr>
-    <th>記事ID</th><th>タイトル</th><th>カテゴリ</th><th>本文</th><th>投稿日</th>
-  </tr>
+      <tr>
+        <th>記事ID</th><th>タイトル</th><th>カテゴリ</th><th>本文</th><th>投稿日</th>
+      </tr>
 
-  <tr>
-    <td>5</td><td>静岡の良さ</td><td>旅行</td><td>富士山</td><td>2022‐02‐02</td>
-  </tr>
-  <tr>
-    <td>4</td><td>静岡の良さaaaaa</td><td>旅行</td><td>富士山</td><td>2022‐02‐02</td>
-  </tr>
-  <tr>
-    <td>3</td><td>静岡の良さ</td><td>旅行</td><td>富士山</td><td>2022‐02‐02</td>
-  </tr>
-  <tr>
-    <td>2</td><td>静岡の良さ</td><td>旅行</td><td>富士山</td><td>2022‐02‐02</td>
-  </tr>
-  <tr>
-    <td>1</td><td>静岡の良さ</td><td>旅行</td><td>富士山</td><td>2022‐02‐02</td>
-  </tr>
-</table>
+      <!-- foreach  $getUserData["id"];-->
+      <?php
+      
+      foreach($getPostData as $row){
+        if($row["category_no"] == 1){
+          $no = '食事';
+        }elseif($row["category_no"] == 2){
+          $no = '旅行';
+        }else{
+          $no = 'その他';
+        }
+        echo  '<tr>';
+        echo  '<td>'. $row["id"]. '</td>';
+        echo  '<td>'. $row["title"] . '</td>';
+        echo  '<td>'. $no .'</td>';
+        echo  '<td>'. $row["comment"] .'</td>';
+        echo  '<td>'. $row["created"] .'</td>';
+        echo '</tr>';
+      }?>
+      
+        
+        
+        
+    </table>
 
     </main>
 
